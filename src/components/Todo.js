63a4586863bc575790172b712d0e1e4e8342b215
unknown <thekajalsharma007@gmail.com>
2018-02-15 18:04:48 +0530

    Removed commented code.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,12 +3,6 @@ import React from 'react';
 class Todo extends React.Component {
 
     render() {
-        // let op = `<ul>`
-        // op += this.props.todoList.map((i) => {
-        //     return `<li>${i.name}</li>`
-        // }).join(' ')
-        // op += `</ul>`
-        // return (<div>{op}</div>);
         return (
             <ul>
                 {this.props.todoList.map(function (listValue) {
